ea2baa229059ced459994592fc24018f55ad70e3
Abdulrahman Alkhatib <abdulrahman@Abdulrahmans-MacBook-Pro-2.local>
2019-09-22 14:00:16 +0300

    initial commit

---
+++ BullsEye/authorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  authorViewController.swift
+//  BullsEye
+//
+//  Created by Abdulrahman Alkhatib on 14.09.2019.
+//  Copyright © 2019 Abdulrahman Alkhatib. All rights reserved.
+//
+
+import UIKit
+
+class authorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
